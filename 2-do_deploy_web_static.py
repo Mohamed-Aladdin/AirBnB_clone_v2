2892b0cf9e9334b4b738afbb11517c50cb253bc3
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-05-03 13:42:20 +0000

    Modify script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,17 +17,17 @@ def do_deploy(archive_path):
         print("NOT PATH")
         return False
 
-    aname = os.path.basename(archive_path)
-    rname = aname.split(".")[0]
+    an = os.path.basename(archive_path)
+    rn = aname.split(".")[0]
 
     put(local_path=archive_path, remote_path="/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(rname))
+    run("mkdir -p /data/web_static/releases/{}".format(rn))
     run("tar -xzf /tmp/{} \
-        -C /data/web_static/releases/{}".format(aname, rname))
-    run("rm /tmp/{}".format(aname))
+        -C /data/web_static/releases/{}".format(an, rn))
+    run("rm /tmp/{}".format(an))
     run("rm -rf /data/web_static/current")
     run("ln -fs /data/web_static/releases/{}/ \
-        /data/web_static/current".format(rname))
+        /data/web_static/current".format(rn))
     run("mv /data/web_static/current/web_static/* /data/web_static/current/")
     run("rm -rf /data/web_static/curren/web_static")
 
