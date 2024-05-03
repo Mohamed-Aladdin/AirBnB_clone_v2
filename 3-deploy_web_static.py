8e22cea81533f8e6a8d7724bd35dc800a8bbd96e
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-05-03 12:18:21 +0000

    Fix script

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,17 +11,17 @@ env.hosts = ["52.6.173.34", "52.3.248.217"]
 env.user = "ubuntu"
 
 
-def do_deploy(arch_path):
+def do_deploy(archive_path):
     """deploy the package fn"""
 
-    if arch_path is None or not os.path.isfile(arch_path):
+    if archive_path is None or not os.path.isfile(archive_path):
         print("NOT PATH")
         return False
 
-    aname = os.path.basename(arch_path)
+    aname = os.path.basename(archive_path)
     rname = aname.split(".")[0]
 
-    put(local_path=arch_path, remote_path="/tmp/")
+    put(local_path=archive_path, remote_path="/tmp/")
     run("mkdir -p /data/web_static/releases/{}".format(rname))
     run("tar -xzf /tmp/{} \
         -C /data/web_static/releases/{}".format(aname, rname))
@@ -51,4 +51,4 @@ def deploy():
     path = do_pack()
     if path is None:
         return False
-    return(do_deploy(path))
+    return (do_deploy(path))
