6d62e78484a2f18a4869299e3cfcfead4a5d0d31
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-05-03 13:08:45 +0000

    Modify script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,13 @@
 # Set up NGINX configuration for the static deployment
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "simple content, to test your Nginx configuration" >> /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "simple content, to test your Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i "/listen 80 default_server;/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-available/default
 sudo ufw allow "Nginx HTTP"
 sudo service nginx restart
 exit 0
